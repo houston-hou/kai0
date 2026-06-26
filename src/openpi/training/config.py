5b64a387a30231582730abe0313f75aab9d9cea3
hdy <88202660@qq.com>
2026-06-26 21:44:14 +0800

    add new config

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -1351,6 +1351,119 @@ _CONFIGS = [
         optimizer=_optimizer.AdamW(),
     ),
 
+    TrainConfig(
+        name="cylinder2reactor_agilex_rtc",
+        model=pi0_config.Pi0RTCConfig(pi05=True, active_arm="left"),
+        data=LerobotAgilexDataConfig(
+            repo_id=None,
+            use_multi_repo=True,
+            repo_ids=[
+                "measure_liquid_full_0605_atomic_cylinder2reactor_trimmed",
+                "measure_liquid_full_0606_atomic_cylinder2reactor_trimmed",
+                "measure_liquid_full_atomic_cylinder2reactor_trimmed",
+            ],
+            root="/mnt/hdy/emchem_pi05/training_data",
+            assets=AssetsConfig(asset_id="cylinder2reactor_agilex"),
+            repack_transforms=_transforms.Group(
+                inputs=[
+                    _transforms.RepackTransform(
+                        {
+                            "images": {
+                                "top_head": "observation.images.cam_high",
+                                "hand_left": "observation.images.cam_left_wrist",
+                                "hand_right": "observation.images.cam_right_wrist",
+                            },
+                            "state": "observation.state",
+                            "actions": "action",
+                            "prompt": "prompt",
+                        }
+                    )
+                ]
+            ),
+            base_config=DataConfig(prompt_from_task=True),
+            use_delta_joint_actions=False,
+        ),
+        freeze_filter=nnx.All(
+            nnx_utils.PathRegex(r".*llm.*"),
+            nnx.Not(nnx_utils.PathRegex(r".*llm.*_1.*")),
+        ),
+        weight_loader=weight_loaders.CheckpointWeightLoader(
+            "/mnt/hdy/kai0-main/weights_cache/openpi-assets/checkpoints/pi05_base/params"
+        ),
+        num_train_steps=100,
+        log_interval=50,
+        save_interval=1000,
+        keep_period=None,
+        num_workers=0,
+        batch_size=16,
+        fsdp_devices=1,
+        wandb_enabled=True,
+        lr_schedule=_optimizer.CosineDecaySchedule(
+            peak_lr=2e-4,
+            warmup_steps=1000,
+            decay_steps=10_000,
+            decay_lr=1e-5,
+        ),
+        optimizer=_optimizer.AdamW(),
+    ),
+
+
+    TrainConfig(
+        name="boil_agilex",
+        model=pi0_config.Pi0Config(pi05=True, active_arm="left"),
+        data=LerobotAgilexDataConfig(
+            repo_id=None,
+            use_multi_repo=True,
+            repo_ids=[
+                "measure_liquid_full_0605_atomic_cylinder2reactor_trimmed",
+                "measure_liquid_full_0606_atomic_cylinder2reactor_trimmed",
+                "measure_liquid_full_atomic_cylinder2reactor_trimmed",
+            ],
+            root="/mnt/hdy/emchem_pi05/training_data",
+            assets=AssetsConfig(asset_id="cylinder2reactor_agilex"),
+            repack_transforms=_transforms.Group(
+                inputs=[
+                    _transforms.RepackTransform(
+                        {
+                            "images": {
+                                "top_head": "observation.images.cam_high",
+                                "hand_left": "observation.images.cam_left_wrist",
+                                "hand_right": "observation.images.cam_right_wrist",
+                            },
+                            "state": "observation.state",
+                            "actions": "action",
+                            "prompt": "prompt",
+                        }
+                    )
+                ]
+            ),
+            base_config=DataConfig(prompt_from_task=True),
+            use_delta_joint_actions=False,
+        ),
+        freeze_filter=nnx.All(
+            nnx_utils.PathRegex(r".*llm.*"),
+            nnx.Not(nnx_utils.PathRegex(r".*llm.*_1.*")),
+        ),
+        weight_loader=weight_loaders.CheckpointWeightLoader(
+            "/mnt/hdy/kai0-main/weights_cache/openpi-assets/checkpoints/pi05_base/params"
+        ),
+        num_train_steps=100,
+        log_interval=50,
+        save_interval=1000,
+        keep_period=None,
+        num_workers=0,
+        batch_size=16,
+        fsdp_devices=1,
+        wandb_enabled=True,
+        lr_schedule=_optimizer.CosineDecaySchedule(
+            peak_lr=2e-4,
+            warmup_steps=1000,
+            decay_steps=10_000,
+            decay_lr=1e-5,
+        ),
+        optimizer=_optimizer.AdamW(),
+    ),
+
     #
     # Fine-tuning DROID configs.
     #
